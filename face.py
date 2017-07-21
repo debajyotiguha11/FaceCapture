ecbc02fe40c26f57b4b9ef3962be370f0ac4875c
Debjyoti Guha <debajyotiguha11@gmail.com>
2017-07-21 08:15:48 +0530

    Add files via upload

---
+++ face.py
@@ -0,0 +1,41 @@
+# Script Name   : face.py
+# Author        : Debjyoti Guha
+# Created       : 21st July 2017
+# Last Modified	: -
+# Version       : 1.0.0
+
+# Modifications : 
+
+# Description   : This Will Scan Your Face and Eyes
+
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv2.VideoCapture(0)
+
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv2.imshow('frame',img)
+    if cv2.waitKey(30) & 0xff == ord('q'):
+    
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
